96867628522cb66bb0d4d5700d0b6bf377b34bee
viper_work <wiperawa@gmail.com>
2020-11-23 15:30:34 +0500

    dynamic-import

--- src/executor.js
+++ src/executor.js
@@ -4,4 +4,5 @@ var babel_presets = require("@babel/preset-react");
 
 var options = JSON.parse(JSON.stringify(PHP.babelOptions));
 options.presets = [babel_presets];
+options.plugins = ["@babel/plugin-syntax-dynamic-import"];
 print(babel.transform(PHP.sourceCode,options).code);
